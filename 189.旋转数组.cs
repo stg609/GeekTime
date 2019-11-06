3cf74458c3b207d7912bf29ccf4c698a30923020
Charlie Chen <stg609@outlook.com>
2019-11-06 22:54:24 +0800

    提交 189 作业

--- 189.旋转数组.cs
+++ 189.旋转数组.cs
@@ -9,19 +9,30 @@ public class Solution
 {
     public void Rotate(int[] nums, int k)
     {
-        //[1,2,3]
-        //[2,3,1]
-        //k=2
-        int[] temp = new int[nums.Length];
-        for (int i = 0; i < nums.Length; i++)
+        int temp = -1;
+        int count = 0;
+        for (int start = 0; count < nums.Length; start++)
         {
-            int j = i + k;
-            if (j >= nums.Length)
+            int current = start;
+            int prevData = nums[current];
+
+            do
             {
-                j = j % nums.Length;
+                //根据当前元素坐标计算目标位置
+                int targetLocation = (current + k) % nums.Length;
+
+                temp = nums[targetLocation];
+                nums[targetLocation] = prevData;
+
+                prevData = temp;
+
+                //由于目标位置的元素被替代了，所以接下来为这个被替代的元素寻找目标位置
+                current = targetLocation;
+
+                //已经执行的元素个数
+                count++;
             }
-            //i=2
-            temp[j] = nums[i];
+            while (current != start);
         }
     }
 }
